42965d06effaff091ab385e0ed175907cd94182c
Sedat Doğan <dogannnsedat@gmail.com>
2022-09-10 15:56:19 +0300

    Player'ın ileri geri hareketi, kameranın baktığı yöne göre değiştirildi

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,15 +6,18 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     public float speed = 5.0f;
+    private GameObject odakNoktasi;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        odakNoktasi = GameObject.Find("OdakNoktasý");//Odak noktasý oyun objesi sahnede aranýp deðiþkene atýldý.
     }
 
     // Update is called once per frame
     void Update()
     {
-        float forwardInput = Input.GetAxis("Vertical");
-        rb.AddForce(Vector3.forward*speed*forwardInput);
+        float forwardInput = Input.GetAxis("Vertical");//Dikey girdiyi tutan deðiþken
+        rb.AddForce(odakNoktasi.transform.forward*speed*forwardInput);
+        //Player objemin rigidbodysine kuvvet ekliyorum.Ancak uygulanan bu kuvvet player objemin baktýðý yöne doðru deðil,odak noktasý objemin baktýðý yöne doðru ekleniyor.
     }
 }
