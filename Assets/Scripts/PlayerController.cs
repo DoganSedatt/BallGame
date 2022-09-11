4c5c87e1ababf42a5b9467463c3bed60daccad61
Sedat Doğan <dogannnsedat@gmail.com>
2022-09-11 16:49:29 +0300

    Güçlendirme objesine,aktiflik durumuna göre bir gösterge eklendi.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
-    public float speed = 5.0f;
     private GameObject odakNoktasi;
-    public bool hasPowerUp;//Güçlendirmenin etkin olup olmadýðýný kontrol edecek.
     private float powerUpStrength = 15.0f;//Ýtme gücü
+
+    public float speed = 5.0f;
+    public GameObject powerActiveObject;
+    public bool hasPowerUp;//Güçlendirmenin etkin olup olmadýðýný kontrol edecek.
+    
     Rigidbody enemyRb;//Düþman rigidbodysine ulaþmak için.
     void Start()
     {
@@ -22,13 +25,18 @@ public class PlayerController : MonoBehaviour
         float forwardInput = Input.GetAxis("Vertical");//Dikey girdiyi tutan deðiþken
         rb.AddForce(odakNoktasi.transform.forward*speed*forwardInput);
         //Player objemin rigidbodysine kuvvet ekliyorum.Ancak uygulanan bu kuvvet player objemin baktýðý yöne doðru deðil,odak noktasý objemin baktýðý yöne doðru ekleniyor.
+        powerActiveObject.transform.position = transform.position + new Vector3(0, -0.4f, 0);
+       
     }
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("powerup"))
         {
+            powerActiveObject.gameObject.SetActive(true);
             hasPowerUp = true;
             Destroy(other.gameObject);
+            StartCoroutine(PowerDownCoroutine());//Ekstra güç olayýný zamanla kaldýran metodu çaðýran metod;
+            
         }
     }
     private void OnCollisionEnter(Collision collision)
@@ -43,5 +51,11 @@ public class PlayerController : MonoBehaviour
             enemyRb.AddForce(awayFromPlayer * powerUpStrength, ForceMode.Impulse);
             //Düþman rb'sine kuvvet ekle=Player ile aradaki mesafe*powerUpStrength,Forcemode ise Impulse(Ýtki anlamýnda).
         }
+    }
+    IEnumerator PowerDownCoroutine()
+    {
+        yield return new WaitForSeconds(5);
+        hasPowerUp = false;
+        powerActiveObject.gameObject.SetActive(false);
     }
 }
